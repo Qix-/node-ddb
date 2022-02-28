138163a4d8a20c9902177f460c55ecca50b40e88
Josh Junon <josh.junon@protonmail.com>
2022-02-28 01:09:32 +0100

    feat: add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+declare const BEGINNING: number;
+declare const RELATIVE: number;
+declare const END: number;
+
+declare function extract(callbacks: {
+	read: (buf: Buffer, sz: number) => number,
+	seek: (pos: number, whence: number) => boolean,
+	tell: () => number,
+	frames: (frames: Buffer[]) => void
+}): void;
+
+declare function extractFrames(buf: Buffer, frames: (frames: Buffer[]) => void): void;
+
+export {
+	BEGINNING,
+	RELATIVE,
+	END,
+	extract,
+	extractFrames
+};
